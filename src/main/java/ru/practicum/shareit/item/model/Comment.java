4885aa26d42f97b6f249fd2276366bcfc09af75b
Daria Leo <dashunkaleo@yandex.ru>
2025-12-25 15:12:20 +0300

    fix with postman tests

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -3,8 +3,10 @@ package ru.practicum.shareit.item.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
@@ -13,6 +15,8 @@ import java.time.LocalDateTime;
 @Table(name = "comments")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
